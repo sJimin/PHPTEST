9a1c425ba2a65684f3a76d4752657682d5866156
sJimin <kgulliver@naver.com>
2016-01-26 23:29:16 +0900

    잉

--- newfile.php
+++ newfile.php
@@ -1,4 +1,35 @@
 <?php 
+		
+interface Man{
+	function move();
+	function _shout();
+}
+	
+class Human implements Man{
+	function move(){
+		echo("Defect user's movement");
+	}
+	function _shout(){
+		echo("Nice to meet you");
+	}
+}
 
-echo "hello github!"
+class Robot implements Man{
+	function move(){
+		echo("Defect Robot's movement");
+	}
+	function _shout(){
+		echo("Nice to meet you");
+	}
+}
+
+$n= new Human();
+$n->move();
+$n->_shout();
+
+$i= new Robot();
+$i-> move();
+$i-> _shout();
+
+//hm님 코딩 해서 연습하래용
 ?>
