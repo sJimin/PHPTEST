fe5ebc813f2874783ab08d16a01b84e7fb91b38c
sJimin <kgulliver@naver.com>
2016-02-08 01:35:38 +0900

    Alpha 1.0 SneakingHide 스텔스 모드 플러그인

--- SneakingHide/src/SneakingHide/SneakingHide.php
+++ SneakingHide/src/SneakingHide/SneakingHide.php
@@ -6,7 +6,7 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerToggleSneakEvent;
 use pocketmine\utils\TextFormat;
-use pocketmine\event\block\SignChangeEvent;
+use pocketmine\entity\Effect;
 
 
 class SneakingHide extends PluginBase implements Listener{
@@ -24,31 +24,21 @@ class SneakingHide extends PluginBase implements Listener{
 	
 	public function onHide(PlayerToggleSneakEvent $event){
 		$player=$event->getPlayer();
-		foreach($player->getLevel()->getPlayers() as $p){
 		if($event->isSneaking()){
-			$p->hidePlayer($player);
-			if($p==$player){
-			   $player->sendMessage(TextFormat::BLUE."스텔스 모드가 켜졌습니다");
+			   $effect= Effect::getEffect(14);
+			   $effect->setDuration (600 * 20);
+			   $effect->setAmplifier (10);
+			   $player->addEffect($effect);
+			   $player->sendMessage(TextFormat::BLUE.$player->getName()."님의 스텔스 모드가 켜졌습니다");
+			} 
+			elseif(!$event->isSneaking()){
+		
+				$player->removeEffect(14);
+				
+		        $player->sendMessage(TextFormat::RED.$player->getName()."님의 스텔스 모드가 꺼졌습니다");
 			}
-			else{
-				$p->showPlayer($player);
-				if($p==$player)
-					$player->sendMessage(TextFormat::BLUE."스텔스 모드가 꺼졌습니다");
-			}
-		}
-		}
 	}
-	
-	public function onSignChange(SignChangeEvent $event){
-		$player=$event->getPlayer();
-		if($event->getLine(0)=="Hide"){
-			$player->hidePlayer($player);
-			$player->sendMessage(TextFormat::BLUE."스텔스 모드가 켜졌습니다");
-			
-		}
-		}
 }
 			
-	
 
 ?>
